a92ccf185a9f9df32447127bf95c34b915f78217
bean89 <ChristofferLien1@gmail.com>
2013-09-10 13:06:49 +0200

    laget javascript og knyttet HTML til Javascript

---
+++ prolangs.js
@@ -0,0 +1,8 @@
+var element_unordered_list = document.getElementById("liste");
+
+function skrivTilUtDataBoks(valgtobj){
+var e_velg = document.getElementById("velg");
+var e_utdataboks = document.getElementById ("utdataboks");
+	e_utdataboks.innerHTML = "Du har valgt " + e_velg.options[valgtobj.selectedIndex].text;
+
+}
