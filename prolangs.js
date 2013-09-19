5dfffb008603903c8abe4f3bebd7dd10a70f443e
bean89 <ChristofferLien1@gmail.com>
2013-09-19 18:06:13 +0200

    lab2 del 2 + kontrollspørsmål

--- prolangs.js
+++ prolangs.js
@@ -4,5 +4,7 @@ function skrivTilUtDataBoks(valgtobj){
 var e_velg = document.getElementById("velg");
 var e_utdataboks = document.getElementById ("utdataboks");
 	e_utdataboks.innerHTML = "Du har valgt " + e_velg.options[valgtobj.selectedIndex].text;
-
+}
+function popupboks(beskjed){
+	alert("funker ikke ennå!")
 }
